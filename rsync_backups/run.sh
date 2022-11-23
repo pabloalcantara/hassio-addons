4a6d04fa0bfce36c0258cf8ee6f9b37eed78a5da
Pablo Alcantara <pablo@pabloalcantara.eti.br>
2022-11-23 16:28:09 -0300

    Update run.sh

--- rsync_backups/run.sh
+++ rsync_backups/run.sh
@@ -2,16 +2,15 @@
 set -e
 
 echo "[Info] Starting Hass.io rsync-backups module backup!"
+echo "[Info] Start rsync-backups"
 
 CONFIG_PATH=/data/options.json
 username=$(jq --raw-output ".username" $CONFIG_PATH)
 password=$(jq --raw-output ".password" $CONFIG_PATH)
-
-echo "[Info] Start rsync-backups"
-
-echo "[Info] Start rsync backups to $rsyncurl"
+sed -i 's/old-text/new-text/g' input.txt
 echo "$username:$password" > /etc/rsyncd.secrets
 chmod 600 /etc/rsyncd.secrets
-cat /etc/rsyncd.secrets
+sed -i 's/auth users = %username%/auth users = $username/g' /data/options.json
+cat /data/options.json
 echo "[Info] rsync-backups Iniciado"
 rsync --daemon --no-detach --config=/etc/rsyncd.conf
