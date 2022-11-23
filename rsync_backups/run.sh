f8b14b9358816028d3e68ce9c3243bd3f4220141
Pablo Alcantara <pablo@pabloalcantara.eti.br>
2022-11-23 16:43:36 -0300

    Update run.sh

--- rsync_backups/run.sh
+++ rsync_backups/run.sh
@@ -9,7 +9,7 @@ username=$(jq --raw-output ".username" $CONFIG_PATH)
 password=$(jq --raw-output ".password" $CONFIG_PATH)
 echo "$username:$password" > /etc/rsyncd.secrets
 chmod 600 /etc/rsyncd.secrets
-sed -i "s/auth users = username/auth users = $username/g" /data/options.json
-cat /data/options.json
+sed -i "s/auth users = username/auth users = $username/g" /etc/rsyncd.conf
+cat /etc/rsyncd.conf
 echo "[Info] rsync-backups Iniciado"
 rsync --daemon --no-detach --config=/etc/rsyncd.conf
