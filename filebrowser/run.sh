ed4973d3ccfdf9328b9d259ff20ad753617ad9bf
Pablo Alcantara <pablo@pabloalcantara.eti.br>
2022-11-25 10:19:07 -0300

    Create run.sh

---
+++ filebrowser/run.sh
@@ -0,0 +1,16 @@
+set -e
+
+echo "[Info] Starting Hass.io filebrowser!"
+echo "[Info] Start rsyncd"
+
+CONFIG_PATH=/data/options.json
+username=$(jq --raw-output ".username" $CONFIG_PATH)
+password=$(jq --raw-output ".password" $CONFIG_PATH)
+hosts_allow=$(jq --raw-output ".hosts_allow" $CONFIG_PATH)
+
+#echo "$username:$password" > /etc/rsyncd.secrets
+#chmod 600 /etc/rsyncd.secrets
+#sed -i "s/auth users = username/auth users = $username/g" /etc/rsyncd.conf
+#sed -i "s/hosts allow = hosts_allow/auth users = $hosts_allow/g" /etc/rsyncd.conf
+echo "[Info] filebrowser Iniciado"
+pause
