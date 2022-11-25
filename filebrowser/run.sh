a27c4060cf8c4dd68b16b7203eeadcd8f0a10e5f
Pablo Alcantara <pablo@pabloalcantara.eti.br>
2022-11-25 10:39:52 -0300

    Update run.sh

--- filebrowser/run.sh
+++ filebrowser/run.sh
@@ -3,10 +3,10 @@ set -e
 echo "[Info] Starting Hass.io filebrowser!"
 echo "[Info] Start filebrowser"
 
-CONFIG_PATH=/data/options.json
-username=$(jq --raw-output ".username" $CONFIG_PATH)
-password=$(jq --raw-output ".password" $CONFIG_PATH)
-hosts_allow=$(jq --raw-output ".hosts_allow" $CONFIG_PATH)
+#CONFIG_PATH=/data/options.json
+#username=$(jq --raw-output ".username" $CONFIG_PATH)
+#password=$(jq --raw-output ".password" $CONFIG_PATH)
+#hosts_allow=$(jq --raw-output ".hosts_allow" $CONFIG_PATH)
 
 #echo "$username:$password" > /etc/rsyncd.secrets
 #chmod 600 /etc/rsyncd.secrets
