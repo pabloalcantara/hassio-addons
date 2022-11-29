6f244f1e853941576f820e12f075a26c14527bca
Pablo Alcantara <pablo@pabloalcantara.eti.br>
2022-11-29 09:18:54 -0300

    Update run.sh

--- filebrowser/run.sh
+++ filebrowser/run.sh
@@ -13,11 +13,11 @@ ln -s /ssl /mnt/ssl
 ln -s /addons /mnt/addons
 ln -s /share /mnt/share
 ln -s /media /mnt/media
-baseurl=echo $hostname |sed "s/-/_/"
-echo $hostname
+baseurl=echo $HOSTNAME |sed "s/-/_/"
+echo $HOSTNAME
 echo $baseurl
 
-echo "[Info] filebrowser Iniciado - $hostname - $baseurl"
+echo "[Info] filebrowser Iniciado - $HOSTNAME - $baseurl"
 echo "[info] first run, login and password is 'admin'"
 
 filebrowser -p 9568 -a 0.0.0.0 -d /addons/filebrowser/database.db -r /mnt --baseurl "/$baseurl"
