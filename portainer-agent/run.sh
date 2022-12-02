326368e01af371beb0fec5d5a94f6d8ffa201e08
Pablo Alcantara <pablo@pabloalcantara.eti.br>
2022-12-02 09:49:38 -0300

    Update run.sh

--- portainer-agent/run.sh
+++ portainer-agent/run.sh
@@ -6,14 +6,8 @@ echo "[Info] Start portainer-agent"
 
 CONFIG_PATH=/data/options.json
 AGENT_SECRET=$(jq --raw-output ".agent_secret" $CONFIG_PATH)
-#password=$(jq --raw-output ".password" $CONFIG_PATH)
-#hosts_allow=$(jq --raw-output ".hosts_allow" $CONFIG_PATH)
+AGENT_SECRET_TIMEOUT=$(jq --raw-output ".agent_secret_timeout" $CONFIG_PATH)
+LOG_LEVEL=$(jq --raw-output ".log_level" $CONFIG_PATH)
 
-#echo "$username:$password" > /etc/rsyncd.secrets
-#chmod 600 /etc/rsyncd.secrets
-#sed -i "s/auth users = username/auth users = $username/g" /etc/rsyncd.conf
-#sed -i "s/hosts allow = hosts_allow/auth users = $hosts_allow/g" /etc/rsyncd.conf
 echo "[Info] portainer-agent Iniciado"
-ls -lh /opt/agent
-tail -f /dev/null
 /opt/agent/agent --port=9001
