251bd3a97c1fa6a0581891d2d2385bd374f3b31e
Pablo Alcantara <pablo@pabloalcantara.eti.br>
2022-12-02 11:03:19 -0300

    Update run.sh

--- portainer-agent/run.sh
+++ portainer-agent/run.sh
@@ -6,7 +6,6 @@ echo "[Info] Start portainer-agent"
 
 CONFIG_PATH=/data/options.json
 AGENT_SECRET=$(jq --raw-output ".agent_secret" $CONFIG_PATH)
-AGENT_SECRET_TIMEOUT=$(jq --raw-output ".agent_secret_timeout" $CONFIG_PATH)
 LOG_LEVEL=$(jq --raw-output ".log_level" $CONFIG_PATH)
 
 echo "[Info] portainer-agent Iniciado"
