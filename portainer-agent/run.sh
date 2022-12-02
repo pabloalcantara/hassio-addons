d3b540a2037652644ef766b005019dcca8cd92ef
Pablo Alcantara <pablo@pabloalcantara.eti.br>
2022-12-02 11:15:25 -0300

    Update run.sh

--- portainer-agent/run.sh
+++ portainer-agent/run.sh
@@ -9,4 +9,5 @@ AGENT_SECRET=$(jq --raw-output ".agent_secret" $CONFIG_PATH)
 LOG_LEVEL=$(jq --raw-output ".log_level" $CONFIG_PATH)
 
 echo "[Info] portainer-agent Iniciado"
-/opt/agent/agent --port=9001
+env
+/opt/agent/agent --port=9001 --secret=$AGENT_SECRET --log-level=$LOG_LEVEL
