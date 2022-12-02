95d1df6cf3b9434c9c1edaa66be5dbb0df2a2d69
Pablo Alcantara <pablo@pabloalcantara.eti.br>
2022-12-02 08:35:29 -0300

    Update run.sh

--- portainer-agent/run.sh
+++ portainer-agent/run.sh
@@ -2,10 +2,10 @@
 set -e
 
 echo "[Info] Starting Hass.io portainer-agent!"
-echo "[Info] Start ortainer-agent"
+echo "[Info] Start portainer-agent"
 
-#CONFIG_PATH=/data/options.json
-#username=$(jq --raw-output ".username" $CONFIG_PATH)
+CONFIG_PATH=/data/options.json
+AGENT_SECRET=$(jq --raw-output ".agent_secret" $CONFIG_PATH)
 #password=$(jq --raw-output ".password" $CONFIG_PATH)
 #hosts_allow=$(jq --raw-output ".hosts_allow" $CONFIG_PATH)
 
@@ -13,7 +13,7 @@ echo "[Info] Start ortainer-agent"
 #chmod 600 /etc/rsyncd.secrets
 #sed -i "s/auth users = username/auth users = $username/g" /etc/rsyncd.conf
 #sed -i "s/hosts allow = hosts_allow/auth users = $hosts_allow/g" /etc/rsyncd.conf
-echo "[Info] rsyncd Iniciado"
+echo "[Info] portainer-agent Iniciado"
 ls -lh /opt/agent
 tail -f /dev/null
 /opt/agent/agent --port=9001
